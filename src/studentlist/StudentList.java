87cc1bd5f9afa58f982c6584de852f46e0f9c734
MohammedNazx <hassan40@sheridancollege.ca>
2023-05-18 19:08:33 -0400

    This is my first commit for StudentList Project on May 17, 2023, at 7:20 pm

---
+++ src/studentlist/StudentList.java
@@ -0,0 +1,17 @@
+
+package studentlist;
+
+/**
+ *
+ * @author nazmul
+ */
+public class StudentList {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
