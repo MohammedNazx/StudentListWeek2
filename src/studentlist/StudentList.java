983bffc0086128878d0a17cbc3e2a6c735545bfa
MohammedNazx <hassan40@sheridancollege.ca>
2023-05-18 19:12:48 -0400

    This is my Second commit for StudentList Project on May 17, 2023, at 7:20 pm

--- src/studentlist/StudentList.java
+++ src/studentlist/StudentList.java
@@ -11,7 +11,7 @@ public class StudentList {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // i added the comment at 7:10
     }
     
 }
